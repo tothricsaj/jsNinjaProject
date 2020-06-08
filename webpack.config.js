f78177ee0a832f990586e1c9d3f6b252a8cb451a
tothricsaj <tothricsaj@gmail.com>
2020-06-08 15:18:00 +0200

    basic config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,11 @@
 const webpack = require('webpack')
+
+module.exports = {
+    entry: {
+        index: './js/index.js'
+    },
+    output: {
+        filename: `[name].js`,
+        path: path.resolve(__dirname, './'),
+    },
+}
