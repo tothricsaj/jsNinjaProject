1e46e2e442140d60dcd787813bd6ce991652dff6
tothricsaj <tothricsaj@gmail.com>
2020-06-08 15:45:05 +0200

    assertion style hack :smile:

--- js/assertion/assert.js
+++ js/assertion/assert.js
@@ -1,8 +1,9 @@
 export const assert = (value, desc) => {
-    let assertContainer = document.querySelector('#result')
     let li = document.createElement('li')
 
-    li.className = value ? 'pass' : 'fail'
+    li.classList.add('assert')
+
+    li.classList.add((value ? 'pass' : 'fail'))
     li.appendChild(document.createTextNode(desc))
-    assertContainer.appendChild(li)
+    document.querySelector('#result').appendChild(li)
 }
