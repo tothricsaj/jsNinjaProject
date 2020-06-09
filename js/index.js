32a3cd860b416bd975ceb2a883943ab352c93240
tothricsaj <tothricsaj@gmail.com>
2020-06-09 23:19:26 +0200

    :poop:

--- js/index.js
+++ js/index.js
@@ -1,12 +1,4 @@
 import { assert } from './assertion/assert.js'
 
-assert(typeof fun === 'function', 'We access the function')
-
-var fun = 3
-
-assert(typeof funn === 'number', 'Now we access the number')
-
-function fun() {}
-assert(typeof funn === 'number', 'Still number')
-
+window.assert = assert
 console.log('index js is loaded')
